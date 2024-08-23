066e2abc73104735a21975b32d9fcf6efb6d1139
Ruth <hyangdan0715@gmail.com>
2024-08-23 10:00:55 +0700

    add all

---
+++ SE411_652115519_Lab01/Iris.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("Iris.csv")
+print (data.head(10))
+
+plt.plot(data.Id, data["SepalLengthCm"], "r--")
+plt.show
